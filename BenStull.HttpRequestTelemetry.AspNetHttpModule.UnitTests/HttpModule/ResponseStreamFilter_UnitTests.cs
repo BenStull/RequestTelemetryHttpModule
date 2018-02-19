479fdcace959abcec79ac062b9c534a3407265f4
Ben Stull <ben@hirelytics.net>
2018-02-19 11:52:09 -0800

    More unit tests for whether or not html is injected

--- BenStull.HttpRequestTelemetry.AspNetHttpModule.UnitTests/HttpModule/ResponseStreamFilter_UnitTests.cs
+++ BenStull.HttpRequestTelemetry.AspNetHttpModule.UnitTests/HttpModule/ResponseStreamFilter_UnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Web;
 using BenStull.HttpRequestTelemetry.AspNetHttpModule.HttpModule;
 using BenStull.HttpRequestTelemetry.AspNetHttpModule.HttpRequest;
@@ -19,6 +20,17 @@ namespace BenStull.HttpRequestTelemetry.Model.UnitTests.HttpModule
                 new List<IHttpResponseTelemetryCollector>(), new Mock<ITelemetryHtmlComposer>().Object);
         }
 
+        private ResponseStreamFilter GetResponseStreamFilter(MemoryStream originalResponseStream,
+            HttpResponseBase response)
+        {
+            var htmlComposer = new Mock<ITelemetryHtmlComposer>();
+            htmlComposer.Setup(a => a.ComposeTelemetryHtml(It.IsAny<IHttpRequestTelemetry>())).Returns("<div></div>");
+
+            return new ResponseStreamFilter(originalResponseStream, new Mock<IHttpRequestTelemetry>().Object,
+                new HttpRequestInformation(new Mock<HttpContextBase>().Object), response,
+                new List<IHttpResponseTelemetryCollector>(), htmlComposer.Object);
+        }
+
         [Fact]
         public void When_CloseCalled_OriginalResponseStreamIsClosed()
         {
@@ -35,6 +47,74 @@ namespace BenStull.HttpRequestTelemetry.Model.UnitTests.HttpModule
         [Fact]
         public void When_WriteCalled_OriginalResponseStreamWriteIsCalled()
         {
+            var originalResponseStream = new MemoryStream();
+            var responseStreamFilter = GetResponseStreamFilter(originalResponseStream);
+
+            responseStreamFilter.Write(new byte[] {0x0}, 0, 1);
+
+            Assert.Equal(1, originalResponseStream.Length);
+        }
+
+        [Fact]
+        public void When_WriteCalled_TelemetryHtmlAppendedBeforeBodyCloseTag_lowercase_nospace()
+        {
+            TelemetryHtmlAppendedBeforeBodyCloseTag_Helper("</body>");
+        }
+
+        [Fact]
+        public void When_WriteCalled_TelemetryHtmlAppendedBeforeBodyCloseTag_lowercase_space()
+        {
+            TelemetryHtmlAppendedBeforeBodyCloseTag_Helper("</ body>");
+        }
+
+        [Fact]
+        public void When_WriteCalled_TelemetryHtmlAppendedBeforeBodyCloseTag_mixedcase_nospace()
+        {
+            TelemetryHtmlAppendedBeforeBodyCloseTag_Helper("</Body>");
+        }
+
+        [Fact]
+        public void When_WriteCalled_TelemetryHtmlAppendedBeforeBodyCloseTag_mixedcase_space()
+        {
+            TelemetryHtmlAppendedBeforeBodyCloseTag_Helper("</ Body>");
+        }
+
+        [Fact]
+        public void When_WriteCalled_TelemetryHtmlNotAppended_IfNotTextHtmlContentType()
+        {
+            var originalResponseStream = new MemoryStream();
+            var response = new Mock<HttpResponseBase>();
+            response.SetupGet(a => a.ContentType).Returns("text/plain");
+            response.SetupGet(a => a.ContentEncoding).Returns(Encoding.UTF8);
+
+            var responseStreamFilter = GetResponseStreamFilter(originalResponseStream, response.Object);
+
+            var bodyCloseTagHtml = "</body>";
+            var bodyCloseTagHtmlBytes = Encoding.UTF8.GetBytes(bodyCloseTagHtml);
+
+            responseStreamFilter.Write(bodyCloseTagHtmlBytes, 0, bodyCloseTagHtmlBytes.Length);
+
+            Assert.Equal(bodyCloseTagHtmlBytes.Length, originalResponseStream.Length);
+        }
+
+        private void TelemetryHtmlAppendedBeforeBodyCloseTag_Helper(string bodyCloseTagHtml)
+        {
+            var originalResponseStream = new MemoryStream();
+            var response = new Mock<HttpResponseBase>();
+            response.SetupGet(a => a.ContentType).Returns("text/html");
+            response.SetupGet(a => a.ContentEncoding).Returns(Encoding.UTF8);
+
+            var responseStreamFilter = GetResponseStreamFilter(originalResponseStream, response.Object);
+
+            var bodyCloseTagHtmlBytes = Encoding.UTF8.GetBytes(bodyCloseTagHtml);
+
+            responseStreamFilter.Write(bodyCloseTagHtmlBytes, 0, bodyCloseTagHtmlBytes.Length);
+
+            originalResponseStream.Seek(0, SeekOrigin.Begin);
+            var modifiedResponse = new StreamReader(originalResponseStream).ReadToEnd();
+
+            Assert.NotEqual(bodyCloseTagHtmlBytes.Length, originalResponseStream.Length);
+            Assert.EndsWith(bodyCloseTagHtml, modifiedResponse);
         }
     }
 }
