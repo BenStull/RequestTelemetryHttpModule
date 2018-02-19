ddeb105a2e35288f384b451800efde30dece71c4
Ben Stull <ben@hirelytics.net>
2018-02-19 12:54:39 -0800

    Add basic telemetry html composition

--- BenStull.HttpRequestTelemetry.Model/Telemetry/TelemetryHtmlComposer.cs
+++ BenStull.HttpRequestTelemetry.Model/Telemetry/TelemetryHtmlComposer.cs
@@ -1,12 +1,42 @@
-﻿using BenStull.HttpRequestTelemetry.Domain.Telemetry;
+﻿using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using BenStull.HttpRequestTelemetry.Domain.Telemetry;
 
 namespace BenStull.HttpRequestTelemetry.Model.Telemetry
 {
     public class TelemetryHtmlComposer : ITelemetryHtmlComposer
     {
+        private const string _htmlTemplate = @"
+<div style='width: 100%'>
+  <table>
+    <tr>
+      <th>Metric</th>
+      <th>Description</th>
+      <th>Value</th>
+      <th>Unit</th>
+    </tr>
+    {0}
+  </table>
+</div>
+";
+
         public string ComposeTelemetryHtml(IHttpRequestTelemetry telemetry)
         {
-            return string.Empty;
+            var rowData =
+                from dataPoint in telemetry.TelemetryDataPoints
+                select new XElement("tr",
+                    new XElement("td", dataPoint.MetricName),
+                    new XElement("td", dataPoint.Description),
+                    new XElement("td", dataPoint.Value),
+                    new XElement("td", dataPoint.Unit));
+
+            var rowDataText = new StringBuilder();
+            foreach (var row in rowData) {
+                rowDataText.Append(row);
+            }
+
+            return string.Format(_htmlTemplate, rowDataText);
         }
     }
 }
